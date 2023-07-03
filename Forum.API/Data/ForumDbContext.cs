015ecbe52d032ebf8ab481d3561a5c5e8abb39da
Konstantin <titarenko42k@gmail.com>
2023-07-03 15:16:59 +0300

    feat(user) : add dbContext and change user and role id type to int

---
+++ Forum.API/Data/ForumDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Forum.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.API.Data
+{
+	public class ForumDbContext : DbContext
+	{
+        public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+	}
+}
